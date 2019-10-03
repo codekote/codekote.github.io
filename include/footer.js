254cfb1f21e0698b9413117abbfdba3833115c47
codekote <n_angelo@mail.ru>
2019-10-03 13:48:32 +0700

    templates

---
+++ include/footer.js
@@ -0,0 +1,12 @@
+document.write(`
+<hr>
+<footer>
+    <nav>
+        <ul>
+            <li><a href="https://twitter.com/codekote" target="_blank">tw</a></li>
+            <li><a href="https://instagram.com/codekote" target="_blank">ig</a></li>
+            <li>&copy; codekote</li>
+        </ul>
+    </nav>
+</footer>
+`);
---
+++ include/header.js
@@ -0,0 +1,13 @@
+document.write(`
+<header>
+    <h2>codekote – железячная логика</h2>
+    <nav>
+        <ul>
+            <li><a href="index.html">Таймлайн</a></li>
+            <li><a href="projects.html">Проекты</a></li>
+            <li><a href="books.html">Книги</a></li>
+        </ul>
+    </nav>
+</header>
+<hr>
+`);
